b35054436278ba2247817339fff02ed96c1fd205
Dmytro <macho.cho98@gmail.com>
2019-07-05 01:36:09 +0300

    added sprite-file

--- _materials_/gulp-starter/gulpfile.js
+++ _materials_/gulp-starter/gulpfile.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 /***** 
 *
 * Install first this modules as devDependencies
@@ -16,6 +17,18 @@ var sassPaths = [
 ];
 
 gulp.task('webserver', function() {
+=======
+var gulp = require("gulp");
+var $ = require("gulp-load-plugins")();
+var connect = require("gulp-connect");
+var browserSync = require("browser-sync").create();
+var sassPaths = ["src/sass"];
+var svgSprite = require('gulp-svg-sprite');
+var rename = require("gulp-rename");
+var ejs = require("gulp-ejs");
+
+gulp.task("webserver", function () {
+>>>>>>> Stashed changes
   connect.server({
     livereload: true,
     port: 3002,
@@ -24,6 +37,7 @@ gulp.task('webserver', function() {
 });
 
 // Static server
+<<<<<<< Updated upstream
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
@@ -63,3 +77,76 @@ gulp.task('watch', ['sass','html', 'js'], function() {
 });
 
 gulp.task('default', ['sass', 'webserver', 'watch']);
+=======
+gulp.task("browser-sync", function () {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+gulp.task("html", function () {
+  gulp.src("*.html").pipe(connect.reload());
+});
+
+gulp.task("js", function () {
+  gulp.src("*.js").pipe(connect.reload());
+});
+
+gulp.task("sprite", function () {
+  gulp
+    .src("src/images/**/*.svg")
+    .pipe(
+      svgSprite({
+        mode: {
+          defs: {
+            example: true
+          }
+        }
+      })
+    )
+    .pipe(gulp.dest("svg-out"));
+});
+
+gulp.task("sass", function () {
+  return gulp
+    .src("src/sass/style.scss")
+    .pipe(
+      $.sass({
+        includePaths: sassPaths
+      }).on("error", $.sass.logError)
+    )
+    .pipe(
+      $.autoprefixer({
+        browsers: ["last 2 versions", "ie >= 9"]
+      })
+    )
+    .pipe(gulp.dest("./css"))
+    .pipe(connect.reload());
+});
+
+gulp.task("watch", ["sass", "html", "js"], function () {
+  gulp.watch(["*.html"], ["html"]);
+  gulp.watch(["src/**/*.html"], ["html"]);
+  gulp.watch(["src/sass/**/*.scss"], ["sass"]);
+  gulp.watch(["src/js/**/*.js"], ["js"]);
+  gulp.watch(["templates/**/*.ejs"], ["templates"]);
+});
+
+gulp.task("templates", function () {
+  gulp
+    .src("./templates/*.ejs")
+    .pipe(
+      ejs({
+        msg: "Hello Gulp!"
+      })
+    )
+    .pipe(rename({
+      extname: ".html"
+    }))
+    .pipe(gulp.dest("./"));
+});
+
+gulp.task("default", ["sass", "webserver", "watch"]);
+>>>>>>> Stashed changes
