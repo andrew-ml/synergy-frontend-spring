00aa6eba251dc86de4c3f518247f5c3e31fead04
Данил Ротань <danil.rotan@gmail.com>
2019-07-19 18:34:39 +0300

    new

--- Danil/homework_5_js /task8.js
+++ Danil/homework_5_js /task8.js
@@ -2,7 +2,7 @@ let bank = 10000;
 let rate = 1;
 let options = ["Орел", "Решка"];
 
-for (i = 0; i <= 5; i++) {
+for (i = 0; i <= 1000000; i++) {
   let casino = Math.floor(Math.random() * options.length);
   let person = Math.floor(Math.random() * options.length);
   if (bank == 0) {
