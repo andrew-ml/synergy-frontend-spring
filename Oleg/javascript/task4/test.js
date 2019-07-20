68a2e4423226ba154917617c808cdebe181d7e59
N1kitchenko <51080332+N1kitchenko@users.noreply.github.com>
2019-07-20 22:51:39 +0300

    Доделал task4 by JS

--- Oleg/javascript/task4/test.js
+++ Oleg/javascript/task4/test.js
@@ -9,7 +9,7 @@ function getEvenNumbers(a, b) {
 
 getEvenNumbers(10, 18);
 
-// Задание №1
+// Задание №2
 
 function arithmeticalMean(a, b) {
     let mean = (a + b) / 2;
@@ -22,15 +22,52 @@ console.log(arithmeticalMean(100, 566));
 let mean = (a, b) => (a + b) / 2;
 console.log(mean(1000, 9000));
 
-// Задание №2
+// Задание №3
 
 function showFiveMessages(message) {
     for (let i = 0; i < 5; i++) {
         setTimeout(function () {
             console.log(message);
-        }, 3000);
+        }, i * 1000);
     };
 };
 
 
 showFiveMessages('Hello');
+
+// Задание №4
+
+function showMessage() {
+    let message = prompt('Ваше сообщение');
+    let time = prompt('Через сколько секунд выывести сообщение');
+    setTimeout(function () {
+        console.log(message);
+    }, time * 1000);
+};
+
+showMessage();
+
+// Задание №5
+
+function runAfterTwoSeconds(callback) {
+    callback();
+};
+
+runAfterTwoSeconds(function () {
+    setTimeout(function () {
+        console.log('Hello');
+    }, 2000);
+});
+
+// Задание №6
+
+function mySetTimeout(message, callback) {
+    console.log(message);
+    callback();
+};
+
+mySetTimeout('Hello', function () {
+    setTimeout(function () {
+        console.log('My friend');
+    }, 2000);
+});
