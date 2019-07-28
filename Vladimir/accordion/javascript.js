c276c730c74c653175dfc194ddd56ab7402dbbe5
shvetsvl <shvetsvl95@gmail.com>
2019-07-28 17:07:52 +0300

    accordion

---
+++ Vladimir/accordion/javascript.js
@@ -0,0 +1,18 @@
+let buttons = document.querySelectorAll("[data-select-page]");
+
+buttons.forEach(function(button) {
+  button.addEventListener("click", function() {
+    const selectPage = this.dataset.selectPage;
+    let page = document.querySelector(`[data-page-id="${selectPage}"]`);
+    console.log(page);
+
+    let pages = document.querySelectorAll("[data-page-id]");
+    pages.forEach(function(page) {
+      page.classList.toggle("active", false);
+    });
+
+    page.classList.toggle("active");
+  });
+});
+
+console.log(buttons);
