8604aac87970143d0dbc41cd739bc84a7dc65da5
shvetsvl <shvetsvl95@gmail.com>
2019-07-28 21:56:53 +0300

    no message

--- Vladimir/accordion/javascript.js
+++ Vladimir/accordion/javascript.js
@@ -4,15 +4,10 @@ buttons.forEach(function(button) {
   button.addEventListener("click", function() {
     const selectPage = this.dataset.selectPage;
     let page = document.querySelector(`[data-page-id="${selectPage}"]`);
-    console.log(page);
-
     let pages = document.querySelectorAll("[data-page-id]");
     pages.forEach(function(page) {
       page.classList.toggle("active", false);
     });
-
     page.classList.toggle("active");
   });
 });
-
-console.log(buttons);
