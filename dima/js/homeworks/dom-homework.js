11b3495398274b1a5f383dd577edab0b4d9590ce
Dmytro <macho.cho98@gmail.com>
2019-08-01 16:39:41 +0300

    added ajax

--- dima/js/homeworks/dom-homework.js
+++ dima/js/homeworks/dom-homework.js
@@ -25,35 +25,44 @@
 //accordeon-widget
 
 
-let buttons = document.querySelectorAll('[data-select-part]');
+// let buttons = document.querySelectorAll('[data-select-part]');
 
-buttons.forEach(function (button) {
-  button.addEventListener('click', function () {
-    let selectPart = this.dataset.selectPart;
-    let content = document.querySelector(`[data-content-id=${selectPart}`);
+// buttons.forEach(function (button) {
+//   button.addEventListener('click', function () {
+//     let selectPart = this.dataset.selectPart;
+//     let content = document.querySelector(`[data-content-id=${selectPart}`);
 
-    let contentParts = document.querySelectorAll(`[data-content-id]`);
+//     let contentParts = document.querySelectorAll(`[data-content-id]`);
 
-    contentParts.forEach(function (content) {
-      content.classList.toggle('active', false);
-    });
-    content.classList.toggle('active');
-  })
-});
+//     contentParts.forEach(function (content) {
+//       content.classList.toggle('active', false);
+//     });
+//     content.classList.toggle('active');
+//   })
+// });
 
 
 
 //modal window
 
-const modalBtn = document.querySelector('.modal-window-btn');
-const modalWindow = document.querySelector('.modal-window');
-const modalBtnClose = document.querySelector('.closed-modal-btn')
+// const modalBtn = document.querySelector('.modal-window-btn');
+// const modalWindow = document.querySelector('.modal-window');
+// const modalBtnClose = document.querySelector('.closed-modal-btn')
 
 
-modalBtn.addEventListener('click', function () {
-  modalWindow.classList.toggle('active', true);
-})
+// modalBtn.addEventListener('click', function () {
+//   modalWindow.classList.toggle('active', true);
+// })
 
-modalBtnClose.addEventListener('click', function () {
-  modalWindow.classList.toggle('active', false);
-})
+// modalBtnClose.addEventListener('click', function () {
+//   modalWindow.classList.toggle('active', false);
+// })
+
+
+// let btn = document.querySelector('.change-font-size');
+
+// btn.addEventListener('click', function () {
+//   btn.innerHTML = '';
+// })
+
+// console.log(btn);
